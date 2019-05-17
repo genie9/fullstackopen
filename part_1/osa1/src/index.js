ec459179accce28133ce71431e424b3e2940d784
Evgenia Lyjina <jenny.lyjina@gmail.com>
2019-05-17 18:15:05 +0300

    Exercise 1.9

--- part_1/osa1/src/index.js
+++ part_1/osa1/src/index.js
@@ -15,14 +15,20 @@ const Stats = ({text, stats}) => (
 )
 
 const Statistics = ({good, neutral, bad}) => {
-  return(<>
-    <Stats text={'hyvä'} stats={good} />
-    <Stats text={'neutraali'} stats={neutral} />
-    <Stats text={'huono'} stats={bad} />
-    <Stats text={'yhteensä'} stats={good+neutral+bad} />
-    <Stats text={'keskiarvo'} stats={(good-bad)/(good+neutral+bad)} />
-    <Stats text={'positiivisia'} stats={`${100*good/(good+neutral+bad)} %`} />
-  </>
+  if (good > 0 | neutral > 0 | bad > 0) {
+    return (
+      <>
+        <Stats text={'hyvä'} stats={good} />
+        <Stats text={'neutraali'} stats={neutral} />
+        <Stats text={'huono'} stats={bad} />
+        <Stats text={'yhteensä'} stats={good+neutral+bad} />
+        <Stats text={'keskiarvo'} stats={(good-bad)/(good+neutral+bad)} />
+        <Stats text={'positiivisia'} stats={`${100*good/(good+neutral+bad)} %`} />
+      </>
+    )
+  }
+  return (
+    < Stats text={'Ei yhtään palautetta annettu'} />
   )
 }
 
@@ -48,7 +54,6 @@ const App = () => {
       <Button handleClick={addToGood(good+1)} text='hyvä' />
       <Button handleClick={addToNeutral(neutral+1)} text='neutraali' />
       <Button handleClick={addToBad(bad+1)} text='bad' />
-      
       <Header text={'Statistiikka'} />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
