a92bce4bbd5bc125dbb46fa7ca2618e85a92c059
Evgenia Lyjina <jenny.lyjina@gmail.com>
2019-05-17 14:24:37 +0300

    Exercise 1.6

--- part_1/osa1/src/index.js
+++ part_1/osa1/src/index.js
@@ -1,68 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Part = (props) => {
-  return (
-    <>
-      <p>{props.name} {props.exercises}</p>
-    </>
-  )
-}
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>        
-    </div>
-   )
-}
+const Header = ({text}) => (
+  <h1>{text}</h1>
+)
 
-const Content = (props) => {
-  return (
-    <div>
-      <Part name={props.parts[0].name} exercises={props.parts[0].exercises}/>
-      <Part name={props.parts[1].name} exercises={props.parts[1].exercises}/>
-      <Part name={props.parts[2].name} exercises={props.parts[2].exercises}/>
-    </div>
-  )
-}
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>{text}</button>
+)
 
-const Total = (props) => {
-  return (
-    <div>
-      <p>yhteensä {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} tehtävää</p>
-    </div>
-  )
-}
+const Stats = ({text, stats}) => (
+  <p>{text} {stats}</p>
+)
 
-const App = () => {  
-  const course = {
-    name: 'Half Stack -sovelluskehitys',
-    parts: [
-      {
-        name: 'Reactin perusteet',
-        exercises: 10
-      },
-      {
-        name: 'Tiedonvälitys propseilla',
-        exercises: 7
-      },
-      {
-        name: 'Komponenttien tila',
-        exercises: 14
-      }
-    ]
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const addToGood = (newValue) => () => {
+    setGood(newValue)
   }
-  course.parts.forEach(part => {
-    console.log(part.name, part.exercises)})
+  const addToNeutral = (newValue) => () => {
+    setNeutral(newValue)
+  }
+  const addToBad = (newValue) => () => {
+    setBad(newValue)
+  }
 
   return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
+    <div>
+      <Header text={'Anna palautetta'} />
+      <Button handleClick={addToGood(good+1)} text='hyvä' />
+      <Button handleClick={addToNeutral(neutral+1)} text='neutraali' />
+      <Button handleClick={addToBad(bad+1)} text='bad' />
+      
+      <Header text={'Statistiikka'} />
+      <Stats text={'hyvä'} stats={good} />
+      <Stats text={'neutraali'} stats={neutral} />
+      <Stats text={'huono'} stats={bad} />
+    </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
